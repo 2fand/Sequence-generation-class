8b314c6b47e5041638725333403b95877fbf2f4d
2fand <167662034+2fand@users.noreply.github.com>
2025-02-22 18:09:43 +0800

    Update HierarchicalNum.h

--- SequenceGeneration/HierarchicalNum.h
+++ SequenceGeneration/HierarchicalNum.h
@@ -1,3 +1,3 @@
 #include <iostream>
 using namespace std;
-unsigned int getPosNum(const unsigned int u_i, const unsigned int index);
+unsigned int getPosNum(const unsigned int u_i, const unsigned int index);//获取阶乘数某一位的数
