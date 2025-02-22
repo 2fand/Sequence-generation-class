55c86625ee75044c80f7f2311fde94be05932ec0
2fand <167662034+2fand@users.noreply.github.com>
2025-02-22 18:09:33 +0800

    Update HierarchicalNum.cpp

--- SequenceGeneration/HierarchicalNum.cpp
+++ SequenceGeneration/HierarchicalNum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "HierarchicalNum.h"
 using namespace std;
-unsigned int getPosNum(const unsigned int u_i, const unsigned int index) {
+unsigned int getPosNum(const unsigned int u_i, const unsigned int index) {//获取阶乘数某一位的数
 	unsigned int digitNum = 1;
 	unsigned int i = index + 1;
 	while (i > 1) {
