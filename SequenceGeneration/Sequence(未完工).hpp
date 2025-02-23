fd35bc81bd8c828865bbd4c8789604d1441c722d
2fand <167662034+2fand@users.noreply.github.com>
2025-02-23 19:08:28 +0800

    Update and rename Sequence.hpp to Sequence(未完工).hpp
    有一些未完工

--- SequenceGeneration/Sequence(未完工).hpp
+++ SequenceGeneration/Sequence(未完工).hpp
@@ -10,39 +10,39 @@ private:
 	unsigned int maxNum;
 	unsigned int swapNum;
 public:
-	Sequence() {
+	Sequence() {//无参构造方法
 		this->maxNum = 1;
 		this->swapNum = 0;
 	}
-	Sequence(const unsigned int len) {
+	Sequence(const unsigned int len) {//长度构造方法
 		this->maxNum = 1;
 		this->swapNum = 0;
 		unsigned int factNum = len;
 		while (this->maxNum *= factNum, factNum-- > 1){}
 		while (len--) { this->items.push_back(0); }
 	}
-	Sequence(const vector<T> f_items) {
+	Sequence(const vector<T> f_items) {//数组构造方法
 		this->items = f_items;
 		this->maxNum = 1;
 		this->swapNum = 0;
 		unsigned int factNum = this->items.size();
 		while (this->maxNum *= factNum, factNum-- > 1){}
 	}
-	void swapVectorItem(const unsigned int index, const unsigned int indexa) {
+	void swapVectorItem(const unsigned int index, const unsigned int indexa) {//交换item数组中的两个元素
 		T tempItem = this->items[index];
 		this->items[index] = this->items[indexa];
 		this->items[indexa] = tempItem;
 	}
-	vector<vector<T>>getSequence() {
+	vector<vector<T>>getSequence() {//获取item数组所能排成的所有唯一序列(未完工)
 
 	}
-	unsigned int sequenceCount() {
+	unsigned int sequenceCount() {//获取所有唯一序列的个书
 		return this->sequenceVector.size();
 	}
-	T pushBackItem(const T item) {
+	T pushBackItem(const T item) {//往item数组的尾部插入元素(未完工)
 		
 	}
-	T popBackItem(const T item) {
+	T popBackItem(const T item) {//删除item数组尾部元素(未完工)
 
 	}
 };
